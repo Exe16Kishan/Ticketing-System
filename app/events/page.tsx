8541a3b5b832f6810d90d5baccfe3cc5ea909266
Kishan <kishanshukla53@gmail.com>
2024-11-04 22:25:10 +0530

    event page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,20 +1,15 @@
-import Card from '@/components/Card';
+"use client"
 import Crousel from '@/components/Crousel';
-import EventFilter from '@/components/EventFilter';
 import React from 'react';
-
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Events = () => {
   return (
-    <div>
-      <Crousel />
-      <div className="grid grid-cols-4  max-w-7xl mx-auto"> {/* Move grid here */}
-        {data.map((item, index) => (
-          <Card key={index} /> 
-        ))}
-      </div>
-    </div>
+    <>
+          <Crousel/>
+      {/* // events card to be render */}
+      {/* fetch events from the db */}
+      
+    </>
   );
 };
 
